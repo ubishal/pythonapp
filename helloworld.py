68e625a8b6c1ba6d4d2076c445f350bfd0e76469
ubishal <bishalup@gmail.com>
2022-02-28 07:08:05 -0600

    Testing automation on CI/CD Pipeline

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,6 @@
 def application(environ, start_response):
     status = '200 OK'
-    output = b'Hello World, I am testing CI/CD Pipeline!'
+    output = b'Hello World, Now I am testing automation with CI/CD Pipeline!'
     response_headers = [('Content-type', 'text/plain'),
     ('Content-Length', str(len(output)))]
     start_response(status, response_headers)
